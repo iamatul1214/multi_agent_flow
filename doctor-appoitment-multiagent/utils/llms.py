b95f24452d7af4b8b1708f83c0cd2ecdb5e35d63
Atul Rai <atulrai@fynd.team>
2026-04-07 07:35:34 +0000

    Integrate groq instead of openAI

--- doctor-appoitment-multiagent/utils/llms.py
+++ doctor-appoitment-multiagent/utils/llms.py
@@ -3,19 +3,19 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 load_dotenv()
-# api_key = os.getenv("GROQ_API_KEY")
-OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
-os.environ["OPENAI_API_KEY"]=OPENAI_API_KEY
+api_key = os.getenv("GROQ_API_KEY")
+# OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+# os.environ["OPENAI_API_KEY"]=OPENAI_API_KEY
 
 class LLMModel:
-    def __init__(self, model_name="gpt-4o"):
+    def __init__(self, model_name="llama-3.3-70b-versatile"):
         if not model_name:
             raise ValueError("Model is not defined.")
         self.model_name = model_name
-        self.openai_model=ChatOpenAI(model=self.model_name)
+        self.groq_api=ChatGroq(model=self.model_name)
         
     def get_model(self):
-        return self.openai_model
+        return self.groq_api
 
 if __name__ == "__main__":
     llm_instance = LLMModel()  
